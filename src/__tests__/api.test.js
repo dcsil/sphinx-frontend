588dc4b45268c120ca31302dbc513a2eed307817
Peter T <petertao9t@gmail.com>
2020-10-20 06:17:49 -0400

    add debug test

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 import { NODE_SERVER } from '../endpoints';
 
 describe('API test suite', () => {
-  it('Test `test` route', async () => {
+  it('Test `/test` route', async () => {
     const { data, status } = await axios.get(`${NODE_SERVER}/test`);
     expect(status).toBe(200);
     expect(data).toBe('Welcome to Sphinx App');
-    console.log(process.env.NODE_ENV);
+  });
+  it('Test `/debug-sentry` route', done => {
+    axios
+      .get(`${NODE_SERVER}/debug-sentry`)
+      .then(res => {
+        done.fail(new Error('This is the error'));
+      })
+      .catch(err => {
+        expect(err.message).toBe('Request failed with status code 500');
+        done();
+      });
   });
 });

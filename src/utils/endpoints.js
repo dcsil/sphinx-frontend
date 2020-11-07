3493954a519e0f48fa065a0c3c1d4a6845503f12
Peter T <petertao9t@gmail.com>
2020-11-07 06:19:21 -0500

    update url

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -3,7 +3,7 @@
 const NODE_SERVER =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : 'https://sphinx-node.herokuapp.com';
+    : 'https://sphinx-backend.herokuapp.com';
 const AI_URL = 'https://sphinx-model.herokuapp.com';
 
 export { NODE_SERVER, AI_URL };

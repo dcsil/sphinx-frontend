342b943f19cf8ae53d8e6bd3ddb92234628aac9c
Hirotaka Ishihara <38371297+JerryIshihara@users.noreply.github.com>
2020-11-28 20:19:15 -0500

    fix heroku (#14)
    * new design
    
    * fake event log
    
    * added traffic redux
    
    * analytics
    
    * analytics complete
    
    * update
    
    * auth temp diabled
    
    * finished front-end
    
    * delete lock
    
    * url
    
    * delete yarn lock

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,10 +1,14 @@
-// const NODE_SERVER =
-//   process.env.NODE_ENV === 'development'
-//     ? 'http://localhost:5000'
-//     : 'https://sphinx-backend.herokuapp.com';
-const NODE_SERVER = 'https://sphinx-backend.herokuapp.com';
+const NODE_SERVER =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://sphinx-backend.herokuapp.com';
+// const NODE_SERVER = 'https://sphinx-backend.herokuapp.com';
 
 // const AI_URL = 'https://sphinx-model.herokuapp.com';
-const AI_URL = 'http://localhost:5000';
+// const AI_URL = 'http://localhost:5000';
+const AI_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5001'
+    : 'https://sphinx-model.herokuapp.com';
 
 export { NODE_SERVER, AI_URL };

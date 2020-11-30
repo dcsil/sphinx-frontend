d9ecaab29a277828217d297612cfc49d18d08beb
Hirotaka Ishihara <38371297+JerryIshihara@users.noreply.github.com>
2020-11-30 11:07:55 -0500

    Update Antd Table (#22)
    * new design
    
    * fake event log
    
    * added traffic redux
    
    * analytics
    
    * analytics complete
    
    * update
    
    * auth temp diabled
    
    * finished front-end
    
    * delete lock
    
    * url
    
    * delete yarn lock
    
    * ssl encryption for geo info
    
    * hide landing page header
    
    * fix table NaN
    
    * refactor & fix barchart
    
    * refactor
    
    * improve code quallity
    
    * add components
    
    * refactored PCA
    
    * solve conflict
    
    * update antd column

--- src/routes/Dashboard/Analytics/IPList/index.js
+++ src/routes/Dashboard/Analytics/IPList/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
-
 import { LABELS } from '../../../../model/traffic.js';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { connect } from 'react-redux';
-
 import { traffic } from '../../../../redux/actions/traffic.js';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import BlockOutlinedIcon from '@material-ui/icons/BlockOutlined';
@@ -15,6 +11,7 @@ import { getColorForPercentage } from '../../../../utils/color';
 import AnomalyLineChart from './LineChart.js';
 import IPHeader from './IPHeader';
 import IPItem from './IPItem';
+import Count from '../../../../components/Counting';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,11 +19,22 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const DROPDOWN = [
+  {
+    text: 'Mark as Benign IP',
+    component: () => <CheckCircleOutlineTwoToneIcon style={{ marginRight: 10, color: 'green' }} />,
+  },
+  {
+    text: 'Add to Block List',
+    component: () => <BlockOutlinedIcon style={{ marginRight: 10, color: 'red' }} />,
+  },
+];
+
 const IPList = ({ malicious, update_label, add2blockList }) => {
   const classes = useStyles();
-  // const [IP, setIP] = React.useState(undefined);
   const [ID, setID] = React.useState(undefined);
   const [state, setState] = React.useState({ All: false });
+
   React.useEffect(() => {
     malicious.forEach(m => {
       if (!(m.id in state)) {
@@ -67,13 +75,76 @@ const IPList = ({ malicious, update_label, add2blockList }) => {
     setState({ ...state, All: false });
   };
 
+  const renderDropdown = () => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          marginBottom: 20,
+        }}
+      >
+        <DropdownButton
+          disabled={Object.keys(state).every(key => !state[key])}
+          as={ButtonGroup}
+          title="Action"
+          id="bg-vertical-dropdown-1"
+        >
+          {DROPDOWN.map((d, i) => (
+            <Dropdown.Item
+              onClick={() => onClickAction(i)}
+              className={classes.button}
+              eventKey={i.toString()}
+            >
+              {d.component()} {d.text}
+            </Dropdown.Item>
+          )).flat(Infinity)}
+        </DropdownButton>
+      </div>
+    );
+  };
+
+  const renderIPItem = () => {
+    return malicious.map(item => (
+      <IPItem
+        key={item.id}
+        item={item}
+        checked={state[item.id] || state['All']}
+        selected={item.id === ID}
+        handleChange={handleChange}
+        update_label={update_label}
+        onClick={() => {
+          if (item.id === ID) {
+            setID(undefined);
+          } else {
+            setID(item.id);
+          }
+        }}
+      />
+    ));
+  };
+
+  const renderAnomalyChart = () => {
+    return (
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          padding: '10px 0px',
+        }}
+      >
+        <AnomalyLineChart malicious={malicious} color={'#ff0000'} />
+      </div>
+    );
+  };
+
   return (
     <ClickAwayListener onClickAway={() => setID(undefined)}>
-      <Paper
-        elevation={3}
-        style={{ width: '100%', padding: 10, display: 'flex', flexDirection: 'column' }}
-        align={'center'}
-      >
+      <Count>
         <span> Total Malicious Count </span>
         <span
           style={{
@@ -81,76 +152,16 @@ const IPList = ({ malicious, update_label, add2blockList }) => {
             color: getColorForPercentage(Math.max(5 - malicious.length, 0) / 5),
           }}
         >
-          {' '}
-          {malicious.length}{' '}
+          {malicious.length}
         </span>
-
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            padding: '10px 0px',
-          }}
-        >
-          <AnomalyLineChart malicious={malicious} color={'#ff0000'} />
-        </div>
-
+        {renderAnomalyChart()}
         <div className={classes.root}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'flex-end',
-              alignItems: 'center',
-              marginBottom: 20,
-            }}
-          >
-            <DropdownButton
-              disabled={Object.keys(state).every(key => !state[key])}
-              as={ButtonGroup}
-              title="Action"
-              id="bg-vertical-dropdown-1"
-            >
-              <Dropdown.Item
-                onClick={() => onClickAction(1)}
-                className={classes.button}
-                eventKey="1"
-              >
-                <CheckCircleOutlineTwoToneIcon style={{ marginRight: 10, color: 'green' }} /> Mark
-                as Benign IP
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() => onClickAction(2)}
-                className={classes.button}
-                eventKey="2"
-              >
-                <BlockOutlinedIcon style={{ marginRight: 10, color: 'red' }} /> Add to Block List
-              </Dropdown.Item>
-            </DropdownButton>
-          </div>
+          {renderDropdown()}
           <IPHeader checked={state['All']} handleChange={handleChange} />
           {malicious.length === 0 && <p style={{ padding: 20, color: '#66666680' }}>No Anomaly</p>}
-          {malicious.map(item => (
-            <IPItem
-              key={item.id}
-              item={item}
-              checked={state[item.id] || state['All']}
-              selected={item.id === ID}
-              handleChange={handleChange}
-              update_label={update_label}
-              onClick={() => {
-                if (item.id === ID) {
-                  setID(undefined);
-                } else {
-                  setID(item.id);
-                }
-              }}
-            />
-          ))}
+          {renderIPItem()}
         </div>
-      </Paper>
+      </Count>
     </ClickAwayListener>
   );
 };
--- src/routes/Dashboard/EventLog/BlockList/index.js
+++ src/routes/Dashboard/EventLog/BlockList/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from './Table.js';
 import { connect } from 'react-redux';
 import { traffic } from '../../../../redux/actions/traffic.js';
 import BlockOutlinedIcon from '@material-ui/icons/BlockOutlined';
+import Count from '../../../../components/Counting';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,11 +15,7 @@ const useStyles = makeStyles(theme => ({
 const BlockList = ({ blockList, blockLogs, remove_block }) => {
   const classes = useStyles();
   return (
-    <Paper
-      elevation={3}
-      style={{ width: '100%', padding: 10, display: 'flex', flexDirection: 'column' }}
-      align={'center'}
-    >
+    <Count>
       <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
         <BlockOutlinedIcon style={{ marginRight: 10 }} /> IP Block List{' '}
       </span>
@@ -36,7 +32,7 @@ const BlockList = ({ blockList, blockLogs, remove_block }) => {
       <div className={classes.root}>
         <Table blockLogs={blockLogs} blockList={blockList} remove={remove_block} />
       </div>
-    </Paper>
+    </Count>
   );
 };
 
